e704d4671c0c34185c2c5d3d17e09bec9956f668
David Charles <ICE-WOLF@users.noreply.github.com>
2016-01-13 13:09:57 -0400

    Update tablegen.py

--- basetable/tablegen.py
+++ basetable/tablegen.py
@@ -1,6 +1,7 @@
+from collections import OrderedDict
 
-
-thefile= open('quers.txt', 'r')
+thefile= open('quers2.txt', 'r')
+#thefile= open('ApplicationsOnline.sql', 'r')
 tableset={}
 x=0
 
@@ -23,44 +24,46 @@ while 1:
             line= thefile.readline()
             currlist=line.split()
             
-    if "--" not in line.split() and "--where" not in line.split() and "--and" not in line.split() and "--create" not in line.split() and "--smrprle," not in line.split()   :#-- was unto the actual word so it wasnt picking up the -- so i hard coded those instances
+    if "--" not in line.split() and "--where" not in line.split() and "--and" not in line.split() and "--create" not in line.split() and "--smrprle," not in line.split()   :
         sqlsyntax= sqlsyntax + line + " "
 
 thefile.close()
 sqlarrray=sqlsyntax.split()
 
 for i in range(0,len(sqlarrray)):
-    if sqlarrray[i].lower()=="from":
+    if sqlarrray[i].lower()=="from" or sqlarrray[i].lower()=="join":
         i=i+1
-		#basicaly it started off as checking between from and where clause but then i found various cases where tables are after the from clause but not before the where clause, if anything just adding to the while and if statementbelow will deal with cases you encounter yourself
-        while(sqlarrray[i].lower()!="where" and sqlarrray[i].lower()!="commit" and sqlarrray[i].lower()!="order" and sqlarrray[i].lower()!="and" and sqlarrray[i].lower()!="minus" and sqlarrray[i].lower()!="create" and sqlarrray[i].lower()!="update" and sqlarrray[i].lower()!="left" and sqlarrray[i].lower()!="inner" and sqlarrray[i].lower()!="(" and sqlarrray[i].lower()!=")" and sqlarrray[i].lower()!="union" and sqlarrray[i].lower()!="select"  and sqlarrray[i]!="(SELECT" and sqlarrray[i]!="(select"  and sqlarrray[i].lower()!="group" and sqlarrray[i].lower()!="loop" and sqlarrray[i].lower()!="insert"):
-            if sqlarrray[i].lower()!="where" and sqlarrray[i].lower()!="commit" and sqlarrray[i].lower()!="order" and sqlarrray[i].lower()!="and" and sqlarrray[i].lower()!="minus" and sqlarrray[i].lower()!="create" and sqlarrray[i].lower()!="update"  and sqlarrray[i].lower()!="left" and sqlarrray[i].lower()!="inner"  and sqlarrray[i].lower()!="(" and sqlarrray[i].lower()!="from" and sqlarrray[i].lower()!=")" and sqlarrray[i].lower()!="union" and sqlarrray[i].lower()!="select" and sqlarrray[i]!="(SELECT" and sqlarrray[i]!="(select" and sqlarrray[i].lower()!="group" and sqlarrray[i].lower()!="loop" and sqlarrray[i].lower()!="insert":
+        while(sqlarrray[i].lower()!="as" and sqlarrray[i].lower()!="on" and sqlarrray[i].lower()!="where" and sqlarrray[i].lower()!="commit" and sqlarrray[i].lower()!="order" and sqlarrray[i].lower()!="and" and sqlarrray[i].lower()!="minus" and sqlarrray[i].lower()!="create" and sqlarrray[i].lower()!="update" and sqlarrray[i].lower()!="left" and sqlarrray[i].lower()!="inner" and sqlarrray[i].lower()!="(" and sqlarrray[i].lower()!=")" and sqlarrray[i].lower()!="union" and sqlarrray[i].lower()!="select"  and sqlarrray[i]!="(SELECT" and sqlarrray[i]!="(select"  and sqlarrray[i].lower()!="group" and sqlarrray[i].lower()!="loop" and sqlarrray[i].lower()!="insert"):
+            if sqlarrray[i].lower()!="as" and sqlarrray[i].lower()!="on" and sqlarrray[i].lower()!="where" and sqlarrray[i].lower()!="commit" and sqlarrray[i].lower()!="order" and sqlarrray[i].lower()!="and" and sqlarrray[i].lower()!="minus" and sqlarrray[i].lower()!="create" and sqlarrray[i].lower()!="update"  and sqlarrray[i].lower()!="left" and sqlarrray[i].lower()!="inner"  and sqlarrray[i].lower()!="(" and sqlarrray[i].lower()!="from" and sqlarrray[i].lower()!=")" and sqlarrray[i].lower()!="union" and sqlarrray[i].lower()!="select" and sqlarrray[i]!="(SELECT" and sqlarrray[i]!="(select" and sqlarrray[i].lower()!="group" and sqlarrray[i].lower()!="loop" and sqlarrray[i].lower()!="insert":
                 tableset[q]=sqlarrray[i]
                 q=q+1
             i=i+1
 
-
+    #print tableset
 result={}
 #print tableset.values()
-for key,value in tableset.items(): # here i just remove duplicates, and some of the duplicates were caused because of ",;)" and others because difference in upper case and lower case
+for key,value in tableset.items():
     
     if value.lower().rstrip("),;")  not in result.values():
         result[key] = value.lower().rstrip("),;")
 
 
 foutput=open("basetables.txt","w")
-foutput.write("TABLE COUNT: " + str(len(result))+"\n\n")
-foutput.write("Percent Error 4%\n\n")
+tblcount=0
 for i in range (0,len(result)):
-    foutput.write(result.values()[i])
-    foutput.write("\n")
-    print result.values()[i]
-    print "\n"
+    if(len(result.values()[i].strip(','))>4):#This line was include to remove aliases, it assumes that all words lower than 4 characters are aliases
+        foutput.write(result.values()[i].strip(','))
+        foutput.write("\n")
+        print result.values()[i].strip(',')
+      
+        tblcount=tblcount+1
 
 
-print len(result)
+#print len(result)
+print tblcount
 
-
+foutput.write("\n\n TABLE COUNT: " + str(tblcount)+"\n\n")
+foutput.write("Percent Error 7%\n\n")
 # 
 #print len(tableset)
 
